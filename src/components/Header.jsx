cf567168573d33d514959cf707ca74bc13fdf79f
ToToRoKir <SebastianPereirok@mail.ru>
2020-10-20 10:22:57 +0300

    Add my react project 2 github

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return ( 
+    <header className='header-grid'>
+        {<img src='https://tipik.ru/wp-content/uploads/2019/05/%D0%A1%D0%BA%D0%B0%D1%87%D0%B0%D1%82%D1%8C-%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B8-240%D1%85320-%D0%B4%D0%BB%D1%8F-%D1%82%D0%B5%D0%BB%D0%B5%D1%84%D0%BE%D0%BD%D0%B0-%D0%B1%D0%B5%D1%81%D0%BF%D0%BB%D0%B0%D1%82%D0%BD%D0%BE001.jpg'></img> }
+    </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return ( 
+      <div className = 'main-content-grid'>
+        <div>
+          Main content
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZadRfF_T-e-7C4nufNJ8T514HyWO5WDG7Ig&usqp=CAU'></img>
+            
+        </div>
+        <div>
+          AVA + DESCRIPTION
+        </div>
+        <div>
+          MY POSTS
+          <div>
+            NEW POSTS
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+            <div>
+              post 3
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
